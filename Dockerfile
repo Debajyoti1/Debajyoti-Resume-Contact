f6f3e05101bfd704fc74b4afa9b8d76d9202d863
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2023-10-28 15:12:02 +0530

    https enabled

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:19-alpine
+FROM node:current-alpine
 
 WORKDIR /app
 
@@ -7,6 +7,8 @@ COPY . .
 RUN npm install
 
 RUN apk add --no-cache gcc musl-dev linux-headers
+
+RUN apt-get update && apt-get upgrade && apt-get install -y nano
 
 EXPOSE 8000
 
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 // Import required modules
 const express = require('express'); // Web framework for Node.js
+const fs = require('fs')
 const mongoose = require('./configs/mongoose'); // MongoDB config file is called to initiate DB connection
 const defaultLog=require('./middlewares/defaultLog')
 const cors = require('cors');
@@ -21,7 +22,16 @@ app.use(defaultLog.defaultLog)
 // Import and use routes defined in separate modules
 app.use('/', require('./routes'));
 
+let key = fs.readFileSync('/certificates','utf-8')
+let cert = fs.readFileSync('/certificates','utf-8')
+const parameters = {
+  key: key,
+  cert: cert
+}
+
 const port = process.env.PORT || 8000; // Use the port defined in the environment variable, or default to port 8000
-app.listen(port, () => {
+
+let server = https.createServer(parameters,app)
+server.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
